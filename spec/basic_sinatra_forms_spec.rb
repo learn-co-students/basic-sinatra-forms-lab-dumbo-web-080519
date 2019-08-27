41ee2f6843761f9d7fa82034e6fb2fafb835f624
Brian Wong <bwong92@gmail.com>
2019-08-27 16:54:55 -0400

    Done.

--- spec/basic_sinatra_forms_spec.rb
+++ spec/basic_sinatra_forms_spec.rb
@@ -9,13 +9,13 @@ describe App do
     it 'renders basketball team form' do
       visit '/newteam'
       expect(page).to have_selector("form")
-      expect(page).to have_field(:name)
-      expect(page).to have_field(:coach)
-      expect(page).to have_field(:pg)
-      expect(page).to have_field(:sg)
-      expect(page).to have_field(:sf)
-      expect(page).to have_field(:pf)
-      expect(page).to have_field(:c)
+      expect(page).to have_field("name")
+      expect(page).to have_field("coach")
+      expect(page).to have_field("pg")
+      expect(page).to have_field("sg")
+      expect(page).to have_field("sf")
+      expect(page).to have_field("pf")
+      expect(page).to have_field("c")
     end
   end
 
