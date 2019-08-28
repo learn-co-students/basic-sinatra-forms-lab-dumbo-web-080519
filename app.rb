760cc49f3d8a76ec86447b33bd175ac0b0b8876d
Sukrit Walia <sukritwalia@pop-os.localdomain>
2019-08-28 09:21:45 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,21 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  get '/team' do
+    @team_hash = {
+      'Team Name: ' => params['team_name'],
+      'Coach: ' => params['coach_name'],
+      'Point Guard: ' => params['pg_name'],
+      'Shooting Guard: ' => params['sg_name'],
+      'Small Forward: ' => params['sf_name'],
+      'Power Forward: ' => params['pf_name'],
+      'Center: ' => params['c_name']
+    }
+    erb :team
+  end
 
 end
